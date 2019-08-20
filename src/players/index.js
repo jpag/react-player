ef497ed9de30239e27c5b21c4d381facf79a1d6a
Jp Gary <jpgary@gmail.com>
2019-08-20 11:45:11 -0400

    disabled all 3rd party video players. Not needed.

--- src/players/index.js
+++ src/players/index.js
@@ -9,15 +9,17 @@ import { DailyMotion } from './DailyMotion'
 import { Mixcloud } from './Mixcloud'
 import { FilePlayer } from './FilePlayer'
 
+// disable all 3rd party players. Adds a bunch to the lib we don't need.
+
 export default [
-  YouTube,
-  SoundCloud,
-  Vimeo,
-  Facebook,
-  Streamable,
-  Wistia,
-  Twitch,
-  DailyMotion,
-  Mixcloud,
+  // YouTube,
+  // SoundCloud,
+  // Vimeo,
+  // Facebook,
+  // Streamable,
+  // Wistia,
+  // Twitch,
+  // DailyMotion,
+  // Mixcloud,
   FilePlayer
 ]
